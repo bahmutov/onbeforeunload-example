df300f3e633ca1c60c4c4769565546d1fd7aea43
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2026-04-27 10:33:16 -0400

    add another stub example

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,16 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
   allowCypressEnv: false,
+  viewportHeight: 100,
+  viewportWidth: 300,
+  defaultBrowser: 'electron',
 
   e2e: {
+    baseUrl: 'http://localhost:3000',
+    supportFile: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
-});
+})
--- cypress/e2e/prevent-default.cy.js
+++ cypress/e2e/prevent-default.cy.js
@@ -1,5 +1,28 @@
+import 'cypress-real-events'
+
 describe('Stub preventDefault', () => {
+  beforeEach(() => {
+    const beforeLoad = cy.stub().as('beforeLoad')
+    const unloadPreventDefault = cy.stub().as('unloadPreventDefault')
+
+    cy.on('window:before:load', (win) => {
+      beforeLoad()
+      cy.stub(win.BeforeUnloadEvent.prototype, 'preventDefault').callsFake(
+        unloadPreventDefault,
+      )
+    })
+  })
+
   it('asks the user before navigating away', () => {
-    cy.visit('/index2.html')
+    cy.visit('/index2')
+    cy.get('@beforeLoad').should('have.been.calledOnce')
+    cy.contains('button', 'Click').realClick()
+    cy.contains('hello world 2')
+    cy.get('@unloadPreventDefault').should('not.have.been.called')
+
+    cy.reload()
+    cy.get('@beforeLoad').should('have.been.calledTwice')
+    cy.get('@unloadPreventDefault').should('have.been.calledOnce')
+    cy.contains('hello world 2')
   })
 })
